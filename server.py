dfd749875ec05924dc127818286cc04245481773
Peter Beard <peter.b.beard+github@gmail.com>
2014-12-03 23:28:52 -0500

    Made config parser a littler nicer. #1 is mostly done now.

--- server.py
+++ server.py
@@ -38,17 +38,17 @@ def update_alarm_queue(queue, alarm_file):
 	pass
 
 # Main listening thread
-def listening_thread():
+def listening_thread(address, port):
 	# Main listening thread
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((TCP_IP, TCP_PORT))
+	s.bind((address, port))
 	s.listen(1)
-	debug('Listening on port %i' % TCP_PORT)
+	debug('Listening on port %i' % port)
 
 	while True:
 		# Accept incoming connection
-		connection, address = s.accept()
-		debug('Connection address: %s:%s' % (address[0], address[1]))
+		connection, in_address = s.accept()
+		debug('Connection address: %s:%s' % (in_address[0], in_address[1]))
 		# Receive data
 		command_string = ''
 		while True:
@@ -64,9 +64,9 @@ def listening_thread():
 		command = Commands.parse_command(command_string)
 		if command:
 			if command[0] == 'S':
-				debug('Alarm %s succeeded on client at %s' % (command[1], address[0]))
+				debug('Alarm %s succeeded on client at %s' % (command[1], in_address[0]))
 			elif command[0] == 'F':
-				debug('Alarm %s failed on client at %s' % (command[1], address[0]))
+				debug('Alarm %s failed on client at %s' % (command[1], in_address[0]))
 			else:
 				debug('Invalid command: %s' % command[0])
 		else:
@@ -74,18 +74,26 @@ def listening_thread():
 		
 	s.close()
 
+# Get settings from file and return a dictionary of settings
+def parse_config_file(filename):
+	p = ConfigParser.RawConfigParser()
+	p.read(filename)
+	# Build the dictionary
+	settings = {}
+	settings['update_interval'] = p.get('Global', 'updateperiod')
+	settings['address'] = p.get('Global', 'address')
+	settings['port'] = int(p.get('Global', 'port'))
+	settings['alarm_file'] = p.get('Global', 'alarmfile')
+
+	return settings
+
 # Read the config file
-p = ConfigParser.RawConfigParser()
-p.read(CONF_FILE)
-QUEUE_UPDATE_INTERVAL_MINUTES = p.get('Global', 'updateperiod')
-TCP_IP = p.get('Global', 'address')
-TCP_PORT = int(p.get('Global', 'port'))
-ALARM_FILE = p.get('Global', 'alarmfile')
+settings = parse_config_file(CONF_FILE)
 
 # Load alarms from file
-alarms = load_alarms(ALARM_FILE)
+alarms = load_alarms(settings['alarm_file'])
 # This alarm runs every so often to make sure the queue gets updated with new alarms
-alarms.enqueue(Alarm.Alarm('queue_update', (QUEUE_UPDATE_INTERVAL_MINUTES,-1,-1,-1,-1)))
+alarms.enqueue(Alarm.Alarm('queue_update', (settings['update_interval'],-1,-1,-1,-1)))
 
 # Add handler for SIGINT
 signal.signal(signal.SIGINT, handle_sigint)
@@ -94,7 +102,7 @@ signal.signal(signal.SIGINT, handle_sigint)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Start listening
-lthread = thread.start_new_thread(listening_thread, ())
+lthread = thread.start_new_thread(listening_thread, (settings['address'], settings['port']))
 
 # Create a client
 good_client = Client.Client('127.0.0.1', 26401, 'goodclient')
@@ -111,9 +119,9 @@ while True:
 	alarm = alarms.peek()
 	wait = alarm.wait_time()
 	# If the wait is long (like, several hours long), wake up periodically to see if there's something fresher on the queue
-	while wait > QUEUE_UPDATE_INTERVAL_MINUTES * 60:
-		debug('Nothing in the queue for another %i seconds. Sleeping for %i minutes before checking again.' % (wait, QUEUE_UPDATE_INTERVAL_MINUTES))
-		time.sleep(QUEUE_UPDATE_INTERVAL_MINUTES * 60)
+	while wait > settings['update_interval'] * 60:
+		debug('Nothing in the queue for another %i seconds. Sleeping for %i minutes before checking again.' % (wait, settings['update_interval']))
+		time.sleep(settings['update_interval'] * 60)
 		wait = alarm.wait_time()
 
 	debug('Next alarm in %i seconds. Sleeping until then.' % wait)
